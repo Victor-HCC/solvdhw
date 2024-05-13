b58eb1ea07277ac65fbb516f616697387aff3418
Victor <vicktorcori@hotmail.com>
2024-05-12 22:09:19 -0300

    Added getters, setters and class Bookstore

--- hw8.js
+++ hw8.js
@@ -23,14 +23,70 @@ class Book {
    * @param {string} genre - The genre of the book.
    */
   constructor(title, author, isbn, price, availability, pages, language, genre) {
-    this.title = title,
-    this.author = author,
-    this.isbn = isbn,
-    this.price = price,
-    this.availability = availability,
-    this.pages = pages,
-    this.language = language,
-    this.genre = genre
+    this._title = title,
+    this._author = author,
+    this._isbn = isbn,
+    this._price = price,
+    this._availability = availability,
+    this._pages = pages,
+    this._language = language,
+    this._genre = genre
+  }
+
+  /**
+   * Getters
+   */
+  get title() {
+    return this._title
+  }
+  get author() {
+    return this._author
+  }
+  get isbn() {
+    return this._isbn
+  }
+  get price() {
+    return this._price
+  }
+  get availability() {
+    return this._availability
+  }
+  get pages() {
+    return this._pages
+  }
+  get language() {
+    return this._language
+  }
+  get genre() {
+    return this._genre
+  }
+
+  /**
+   * Setters
+   */
+  set title(title) {
+    this._title = title
+  }
+  set author(author) {
+    this._author = author
+  }
+  set isbn(isbn) {
+    this._isbn = isbn
+  }
+  set price(price) {
+    this._price = price
+  }
+  set availability(availability) {
+    this._availability = availability
+  }
+  set pages(pages) {
+    this._pages = pages
+  }
+  set language(language) {
+    this._language = language
+  }
+  set genre(genre) {
+    this._genre = genre
   }
 
 }
@@ -46,9 +102,35 @@ class User {
    * @param {string} email - The email of the user.
    */
   constructor(id, name, email) {
-    this.id = id,
-    this.name = name,
-    this.email = email
+    this._id = id,
+    this._name = name,
+    this._email = email
+  }
+
+  /**
+   * Getters
+   */
+  get id() {
+    return this._id
+  }
+  get name() {
+    return this._name
+  }
+  get email() {
+    return this._email
+  }
+
+  /**
+   * Setters
+   */
+  set id(id) {
+    this._id = id
+  }
+  set name(name) {
+    this._name = name
+  }
+  set email(email) {
+    this._email = email
   }
 
   /**
@@ -71,8 +153,25 @@ class Cart {
    * @param {User} user - The user associated with the cart.
    */
   constructor(user) {
-    this.user = user
-    this.items = []
+    this._user = user
+    this._items = []
+  }
+
+  /**
+   * Getters
+   */
+  get user() {
+    return this._user
+  }
+  get items() {
+    return this._items
+  }
+
+  /**
+   * Setters
+   */
+  set user(user) {
+    this._user = user
   }
 
   /**
@@ -80,7 +179,7 @@ class Cart {
    * @param  {...Book} books - The books to be added to the cart.
    */
   addBooks(...books) {
-    this.items.push(...books)
+    this._items.push(...books)
   }
 
   /**
@@ -88,15 +187,20 @@ class Cart {
    * @param {Book} book - The book to be removed from the cart.
    */
   removeBook(book) {
-    this.items = this.items.filter(bookCart => bookCart.isbn !== book.isbn)
+    this._items = this.items.filter(bookCart => bookCart.isbn !== book.isbn)
   }
 
   /**
    * Calculates the total price of the books in the cart.
+   * If the price is equal exceeds 100 applies a 5% discount
    * @returns {number} The total price of the books in the cart.
    */
   totalPrice() {
-    return this.items.reduce((acc, book) => acc + book.price, 0)
+    let total = this.items.reduce((acc, book) => acc + book.price, 0)
+    if(total >= 100) {
+      total *= .95
+    }
+    return parseFloat(total.toFixed(2))
   }
 }
 
@@ -110,9 +214,22 @@ class Order {
    * @param {Cart} cart - The cart of the user.
    */
   constructor(user, cart) {
-    this.user = user
-    this.books = cart.items
-    this.total = cart.totalPrice()
+    this._user = user
+    this._books = cart.items
+    this._total = cart.totalPrice()
+  }
+
+  /**
+   * Getters
+   */
+  get user() {
+    return this._user
+  }
+  get books() {
+    return this._books
+  }
+  get total() {
+    return this._total
   }
 }
 
@@ -136,7 +253,6 @@ const book9 = new Book("Crime and Punishment", "Fyodor Dostoevsky", "97806797345
 
 const user1 = new User(1, 'Karl', 'karl@email.com')
 const user2 = new User(2, 'John', 'john@email.com')
-const user3 = new User(3, 'Karen', 'karen@email.com')
 
 const cartUser1 = new Cart(user1)
 cartUser1.addBooks(book1, book3, book5)
@@ -145,6 +261,7 @@ const orderUser1 = user1.makeOrder(cartUser1)
 console.log(cartUser1);
 console.log(cartUser1.totalPrice());
 console.log(orderUser1);
+console.log(orderUser1.total);
 
 /*
 ### **Part 3: Demonstration**
@@ -154,8 +271,10 @@ console.log(orderUser1);
 3. **Polymorphism**: Utilize polymorphism by treating different types of books (e.g., fiction, non-fiction) uniformly when users add them to the cart.
 */
 
+
 const karl = new User(1, 'Karl', 'karl@email.com')
 const john = new User(2, 'John', 'john@email.com')
+const karen = new User(3, 'Karen', 'karen@email.com')
 
 const books = [book1, book2, book3, book4, book5, book6, book7, book8, book9]
 
@@ -206,6 +325,13 @@ karlCart.addBooks(book8, book9); //Non-fiction books
 const karlOrder = karl.makeOrder(karlCart);
 console.log(karlOrder);
 
+
+//Karen many book to get the discount
+const cartKaren = new Cart(karen)
+cartKaren.addBooks(book1, book2, book5, book6, book3, book9, book8, book9, book4)
+console.log(cartKaren);
+console.log(cartKaren.totalPrice());
+
 /*
 ### **Part 4: Documentation**
 
@@ -253,3 +379,90 @@ class Order {
 
 Implement additional features such as searching for books, applying discounts, handling payments, or integrating a database to store book and user information.
 */
+
+
+/**
+ * Represents a bookstore
+ */
+class Bookstore {
+  /**
+   * Creates an instance of Bookstore
+   * @param {string} name - The name of the bookstore
+   * @param {string} location - The location of the bookstore
+   */
+  constructor(name, location) {
+    this._name = name,
+    this._location = location,
+    this._inventory = []
+  }
+
+  /**
+   * Getters
+   */
+  get name() {
+    return this._name
+  }
+  get location() {
+    return this._location
+  }
+  get inventory() {
+    return this._inventory
+  }
+
+  /**
+   * Setters
+   */
+  set name(name) {
+    this._name = name
+  }
+  set location(location) {
+    this._location = location
+  }
+
+  /**
+   * Adds books to the inventory.
+   * @param  {...Book} books - The books to be added to the inventory.
+   */
+  addBooks(...books) {
+    this._inventory.push(...books)
+  }
+
+  /**
+   * Removes a book from the inventory.
+   * @param {Book} book - The book to be removed from the inventory.
+   */
+  removeBook(book) {
+    this._inventory = this.inventory.filter(bookInventory => bookInventory.isbn !== book.isbn)
+  }
+
+  /**
+   * Search a book by the title
+   * @param {string} title - The title of the searched book.
+   * @returns {Book} The book searched or a message if there is no result.
+   */
+  searchByTitle(title) {
+    const result = this.inventory.filter(book => book.title === title)
+
+    return result.length > 0 ? result[0] : `No result for ${title}`
+  }
+
+  /**
+   * Search a book by its ISBN
+   * @param {isbn} isbn - The ISBN of the searched book.
+   * @returns {Book} The book searched or a message if there is no result.
+   */
+  searchByISBN(isbn) {
+    const result = this.inventory.filter(book => book.isbn === isbn)
+
+    return result.length > 0 ? result[0] : `No result for ${isbn}`
+  }
+}
+
+//Instance of Bookstore and tests for the search functions
+const bestBookstore = new Bookstore('The Best Bookstore', 'Spring Av. 123')
+bestBookstore.addBooks(...books)
+console.log('Inventory: ', bestBookstore.inventory);
+console.log(bestBookstore.searchByTitle('1984'));
+console.log(bestBookstore.searchByTitle('Misery'));
+console.log(bestBookstore.searchByISBN('9781408855652'));
+console.log(bestBookstore.searchByISBN('9781408855654'));
